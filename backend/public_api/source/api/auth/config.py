53a5bf4214f8c8b6b314a3499725f3cec124a73c
literally-bug-creator <literallybugcreator@gmail.com>
2025-05-24 00:57:47 +0300

    Patch `public_api/auth` section
    1. Move `auth` service from `services` folder
    2. Move `AuthSettings` from `public_api` global config
    3. Delete `__init__.py`

--- backend/public_api/source/api/auth/config.py
+++ backend/public_api/source/api/auth/config.py
@@ -1,5 +1,8 @@
 from enum import StrEnum
 
+from pydantic import Field
+from pydantic_settings import BaseSettings
+
 PREFIX = "/auth/jwt"
 
 
@@ -7,3 +10,15 @@ class EPath(StrEnum):
     ME = "/me"
     LOGIN = "/login"
     REGISTER = "/register"
+
+
+class AuthSettings(BaseSettings):
+    algorithm: str = Field(default="RS256", validation_alias="JWT_ALGORITHM")
+    private_key_path: str = Field(validation_alias="JWT_PRIVATE_KEY_PATH")
+    public_key_path: str = Field(validation_alias="JWT_PUBLIC_KEY_PATH")
+    token_lifetime: int = Field(validation_alias="JWT_TOKEN_LIFETIME")
+    token_type: str = Field(default="Bearer", validation_alias="JWT_TOKEN_TYPE")
+
+
+def get_auth_settings() -> AuthSettings:
+    return AuthSettings()  # type: ignore
