3621822b056270e4b8f0b2a4586c92f15793494d
literally-bug-creator <literallybugcreator@gmail.com>
2025-05-23 20:50:52 +0300

    Update `alembic` `.env`

--- backend/public_api/source/database/migrations/env.py
+++ backend/public_api/source/database/migrations/env.py
@@ -1,6 +1,7 @@
 from logging.config import fileConfig
 
 from alembic import context
+from database import Base, get_db_url
 from sqlalchemy import engine_from_config, pool
 
 # this is the Alembic Config object, which provides
@@ -14,13 +15,13 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+
+target_metadata = Base.metadata
+config.set_main_option("sqlalchemy.url", get_db_url())
+
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
 # ... etc.
 
 
